e308dbbb3d89495fc13e73aa4de53d7a98f3bd0d
0xEagle <84868283+0xEagle1337@users.noreply.github.com>
2023-08-27 23:16:13 +0200

    added tables for new functionalities

--- garage.sql
+++ garage.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `owned_vehicles` ADD `impound` tinyint(1) NOT NULL DEFAULT 0;
 ALTER TABLE `owned_vehicles` ADD `park` tinyint(1) NOT NULL DEFAULT 0;
+ALTER TABLE `owned_vehicles` ADD `engine_health` float DEFAULT 999.999;
+ALTER TABLE `owned_vehicles` ADD `body_health` float DEFAULT 999.999;
+ALTER TABLE `owned_vehicles` ADD `deformation` longtext;
+ALTER TABLE `owned_vehicles` ADD `fuel_level` float DEFAULT 99.99;
